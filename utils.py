146c0b87400c8f492a687587b87139d904dfaf23
Thiago de Figueiredo <fmattos.thiago@gmail.com>
2026-07-09 16:08:44 +0000

    criei o utils para melhorar a organização do programa

---
+++ utils.py
@@ -0,0 +1,28 @@
+import datetime
+
+def cabecalho():
+    print("="*22)
+    print(" CONTROLE FINANCEIRO ")
+    print("="*22)
+
+def entrada(texto):
+    valor = input(texto).strip()
+    while not valor:
+        print ("Digite uma opção válida! ")
+        valor = input(texto).strip()
+    return valor
+
+def validacaoTipo(texto):
+    valorTipo = input(texto) 
+    while valorTipo not in ("entrada", "saida"):
+            print("Opção não é válida!") 
+            valorTipo = input("Digite entrada/saida para a movimentação: " )
+    return valorTipo
+
+def escolherData():
+    data = datetime.datetime.now()
+    dataFormatada = data.strftime("%d/%m/%Y")
+    return dataFormatada
+
+def formatarMoeda(valor):
+    return f"{valor:.2f}".replace(".", ",")
